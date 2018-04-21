fa650a4fa54b684faee6164c9381795bc997a9ce
Hannah Lyon <hannahlyon@Hannahs-MacBook-Pro.local>
2018-04-21 19:44:02 +0800

    Write new json with tweet and sentiment analysis

--- sentiment_tokenization.py
+++ sentiment_tokenization.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Sat Apr 21 14:03:41 2018
+Code for sentiment analysis of tweets and tokenization
 
-@author: hannahlyon
 """
 
 from textblob import TextBlob
+import tweepy
+import pickle
