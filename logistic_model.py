fdb7d39d4afcec5b9a3362bd632c045df68a5ef6
Hannah Lyon <hannahlyon@Hannahs-MacBook-Pro.local>
2018-05-13 19:02:43 +0800

    adjusted to use labeled_data.json

--- logistic_model.py
+++ logistic_model.py
@@ -3,9 +3,7 @@ Links:
     https://zlatankr.github.io/posts/2017/03/06/mle-gradient-descent
     https://machinelearningmastery.com/implement-logistic-regression-stochastic-gradient-descent-scratch-python/
     
-
 '''
-
 
 import json
 import math
@@ -14,6 +12,7 @@ import random
 from textblob import TextBlob
 import sentiment_tokenization
 
+'''
 def get_labels(combined_data, csv_f):
     # combined data has "timestamp", "tweet", "sentiment"
     # csv_f has the label indicating price movement at each minute
@@ -27,7 +26,7 @@ def get_labels(combined_data, csv_f):
                 tweet["label"] = line[-1]
                 
     return combined_data
-
+'''
 
 def split_data(data):
     # divide data into training, validation, and test sets
@@ -52,32 +51,30 @@ def logistic_func(w, x):
     return f
 
 
-def mle(x, y, w, b, cost_history):
-    # cost_history is a list of the cost after each iteration of SGD
-    temp = 0
-    m = len(x)
-    for i in range(m):
-        temp += y[i] * math.log(logistic_func(w, x[i], b[i])) + \
-        (1 - y[i]) * math.log( 1 - logistic_func(w, x[i], b[i]))
-    J = -(1 / m) * temp
-    cost_history.append(J)
-    return J, cost_history
+def mle(x, label, w):
+    
+    cost = -1 * (label * math.log(logistic_func(w,x)) + (1-label) * math.log(1 - logistic_func(w,x)))
+
+    return cost
 
 
-def SGD(data, y, a, w):
+def SGD(data, labels, a, w):
     
     #w = [random.random() for i in range(len(data[0] + 1))]
     
     # shuffle points
     random.shuffle(data)
     
+    cost = []
+    
     for d in range(len(data)):
         
         update = []
+        cost.append(mle(d, labels[d], w))
         
         for i in range(len(w)):
             # need a temp list to store updates to each item in w
-            temp =  w - a * (logistic_func(w, data[d]) - y[d]) * data[i]
+            temp =  w - a * (logistic_func(w, data[d]) - labels[d]) * data[i]
             update.append(temp)
             
         # update all items in w simultaneously
@@ -85,28 +82,30 @@ def SGD(data, y, a, w):
         w = w - a * update
 
     return w
-    
-
-def predict(x, w):
-    pass
 
 
 def main():
-    combined_data = sentiment_tokenization.combined_data
-    print(combined_data[0])
+    #combined_data = sentiment_tokenization.combined_data
+    #print(combined_data[0])
+    
+    # or just use labeled_tweets.json
+    labeled_data = json.loads('labeled_tweets.json')
+    print(labeled_data[0])
     
     # get just the relevant variables we are using 
     data = []
     labels = []
-    for d in combined_data:
-        data.append([1, d["sentiment"].polarity ,  d["sentiment"].subjectivity, d["price"]])
+    for d in labeled_data:
+        data.append([1, d["sentiment"][0] ,  d["sentiment"][1], d["price"]])
         labels.append(d["label"])
+    
+    print(data[0])
     
     epochs = 1
     a = 0.001
     w = np.zeros(len(data[0])+1)
     
-    for e in range(epochs):
-        w = SGD(data, labels, a, w)
+    #for e in range(epochs):
+    #    w = SGD(data, labels, a, w)
     
 main()
