328f8c203abc159ad6e3772a082a6e0673f21feb
Hannah Lyon <36256017+hannahlyon@users.noreply.github.com>
2018-05-16 11:41:26 +0800

    pca with logistic regression

--- bitcoin_tweet_PCA.py
+++ bitcoin_tweet_PCA.py
@@ -8,6 +8,8 @@ import json
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.model_selection import train_test_split
 
 
 # data in format: [1, d["sentiment"][0] , d["sentiment"][1]]
@@ -41,19 +43,56 @@ def pca_projection(data):
     return principal_df, explained_variance
     
 
+def logistic_reg(data, labels):
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size = 0.2)
+    
+    logreg = LogisticRegression( max_iter = 1000)
+    logreg.fit(x_train, y_train)
+    
+    #predictions = logisticRegr.predict(x_test)
+    
+    score = logreg.score(x_test, y_test)
+
+    return score
+
+
+def sgd(data, labels):
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size = 0.2)
+    sgd = SGDClassifier(loss='log', penalty = 'l2', alpha = 0.0001, shuffle = True, max_iter = 1000)
+    sgd.fit(x_train, y_train)
+    score = sgd.score(x_test, y_test)
+    return score
+
+
 def main():
     
     labeled_data = []
     with open('labeled_tweets.json') as json_file:
         for line in json_file:
             labeled_data.append(json.loads(line))
-    print(labeled_data[0])
+    
     
     data = []
     labels = []
     for d in labeled_data:
-        data.append([1, d["sentiment"][0] ,  d["sentiment"][1]]) # no d["price"]
+        data.append([1, d["sentiment"][0] ,  d["sentiment"][1]]) 
         labels.append(d["label"])
+        
+    data = standardize_data(data)
+    
+    principal_df, explained_variance = pca_projection(data)
+    #print(principal_df)
  
-
-# apply logistic regression to the transformed data?
+    # combine PC and labels
+    # turn labels into a df- need to standardize?? 
+    labels_df = pd.DataFrame(labels)
+    
+    #run logistic reg
+    score = logistic_reg(principal_df, labels_df)
+    print('Score:', score)
+    
+    
+    score_2 = sgd(principal_df, labels_df)
+    print('Score 2:', score_2)
+    
+main()
